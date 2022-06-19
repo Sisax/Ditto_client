fb98353afb43fc02d135bdb49411797d7766c4ba
Sisax <tamas.sisa@gmail.com>
2022-06-19 16:19:15 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { WEBSOCKET_URL } from './config';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { flushSync } from 'react-dom';
+
+const client = new W3CWebSocket(`${WEBSOCKET_URL}`);
 
 function App() {
+  const [isConnected, setIsConnected]: any = useState(false);
+  const [connectionMessage, setConnectionMessage]: any = useState('Not connected');
+  const [messages, setMessages]: any = useState([]);
+
+  useEffect(() => {
+    client.onmessage = (message: any) => {
+      const messageData = JSON.parse(message.data);
+      console.log(messageData);
+      setMessages([...messages, messageData]);
+    }
+  }, []);
+
+  useEffect(() => {
+    setIsConnected(true);
+    setConnectionMessage('Yay, its connected');
+  }, [client.onopen]);
+
+  function buttonHandler(message: string) {
+    client.send(JSON.stringify({
+      type: "message",
+      message
+    }));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Chat app</h1>
+      <p>{connectionMessage}</p>
+      <ul className="chat">
+        <ChatItem />
+      </ul>
+      <button disabled={isConnected ? false : true} onClick={() => buttonHandler('somebalue') }>Send</button>
     </div>
   );
+}
+
+function ChatItem() {
+  return (
+    <li>hujuj</li>
+  )
 }
 
 export default App;
